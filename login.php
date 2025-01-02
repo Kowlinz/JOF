aed12bbce8ad490f2cac23d5e50c62ddc3214e3b
Andrei <abcdrei02@gmail.com>
2025-01-02 21:41:25 +0800

    Refactor login.php by removing unnecessary comments and whitespace for cleaner code. Streamlined the login form structure while maintaining functionality.

--- login.php
+++ login.php
@@ -12,11 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jack of Fades | Login</title>
-    
-    <!-- Link to Bootstrap CSS for styling -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
-    <!-- Link to custom CSS -->
     <link rel="stylesheet" href="css/login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
@@ -63,7 +59,6 @@
                         $result_customer = mysqli_query($conn, $sql_customer);
                         $user_customer = mysqli_fetch_array($result_customer, MYSQLI_ASSOC);
                         
-                        
                         // Validate the user based on the role
                         if ($user_customer) {
                             if (password_verify($password, $user_customer["password"])) {
@@ -83,32 +78,21 @@
                 ?>
 
                 <h2 class="login-header">Login</h2>
-
-                <!-- Login form -->
                 <form action="login.php" method="post">
                     <div class="form-group">
-                        <!-- Email input -->
                         <input type="email" name="email" class="form-control" placeholder="Email" required>
                     </div>
-
                     <div class="form-group">
-                        <!-- Password input -->
                         <input type="password" name="password" class="form-control" placeholder="Password" required>
                     </div>
-
                     <div class="forgot-password">
-                        <!-- Link to Forgot Password page -->
                         <a href="forgot-password.php">Forgot Password?</a>
                     </div>
-
                     <div class="form-btn">
-                        <!-- Submit button for the form -->
                         <input type="submit" value="Login" name="Login" class="btn btn-primary">
                     </div>
                 </form>
-
                 <div>
-                    <!-- Link to registration page for new users -->
                     <p>New User? <a href="registration.php">Register</a></p>
                 </div>
             </div>

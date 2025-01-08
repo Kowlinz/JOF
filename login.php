ad52a6350e9374ff8fba53b51427234019ccee52
Andrei <abcdrei02@gmail.com>
2025-01-08 08:44:07 +0800

    Added Bootstrap Icons and back button to login and registration page

--- login.php
+++ login.php
@@ -13,6 +13,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jack of Fades | Login</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/login.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
@@ -50,6 +51,11 @@
             </div>
 
             <div class="login-container fade-in">
+                <div class="d-flex justify-content-start mb-4">
+                    <a href="index.php" class="btn btn-warning text-dark fw-bold">
+                        <i class="bi bi-arrow-left"></i> Back
+                    </a>
+                </div>
 
                 <?php
                         if (isset($_POST["Login"])) {
@@ -81,7 +87,6 @@
                     }
                 ?>
 
-                <button onclick="window.location.href='index.php'">Back</button>
                 <h2 class="login-header">Login</h2>
                 <form action="login.php" method="post">
                     <div class="form-group">
--- registration.php
+++ registration.php
@@ -12,12 +12,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jack of Fades | Registration</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="css/login.css">
 </head>
 <body>
     <div class="login-background" style="background-image: url(css/images/barbershop.jpg);">
         <div class="container">
             <div class="login-container fade-in">
+                <!-- Add back button -->
+                <div class="d-flex justify-content-start mb-4">
+                    <a href="index.php" class="btn btn-warning text-dark fw-bold">
+                        <i class="bi bi-arrow-left"></i> Back
+                    </a>
+                </div>
 
                 <?php 
                     // validate the submit button

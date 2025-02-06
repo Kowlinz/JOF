1e8fdb6aaf6082692908d9f6a67e267fa90f9e01
Kowlinz <yvescolinnaig@gmail.com>
2025-02-07 00:24:00 +0800

    adjust notification count from all pending customer to pending customer without an assigned barber for a_history

--- admin/a_history.php
+++ admin/a_history.php
@@ -22,11 +22,16 @@
 <body>
     <?php include 'db_connect.php'; 
     
-    // Fetch the number of pending appointments
-    $notificationQuery = "SELECT COUNT(*) AS pending_count FROM appointment_tbl WHERE status = 'Pending'";
+    // Fetch the number of pending appointments without an assigned barber
+    $notificationQuery = "
+        SELECT COUNT(*) AS notif_count 
+        FROM appointment_tbl a
+        LEFT JOIN barb_apps_tbl b ON a.appointmentID = b.appointmentID
+        WHERE a.status = 'Pending' AND b.barberID IS NULL";
+
     $notificationResult = mysqli_query($conn, $notificationQuery);
     $notificationData = mysqli_fetch_assoc($notificationResult);
-    $pendingCount = $notificationData['pending_count'];
+    $notificationCount = $notificationData['notif_count'];
 
     ?>
 
@@ -233,8 +238,8 @@
                         <i class="fa-solid fa-users fa-fw me-3"></i>
                         <span>Appointment</span>
                     </div>
-                    <?php if ($pendingCount > 0): ?>
-                        <span class="badge bg-danger rounded-pill"><?php echo $pendingCount; ?></span>
+                    <?php if ($notificationCount > 0): ?>
+                        <span class="badge bg-danger rounded-pill"><?php echo $notificationCount; ?></span>
                     <?php endif; ?>
                 </a>
 

0092c68406940a853b90596f8c4bbac969db04ce
Kowlinz <yvescolinnaig@gmail.com>
2025-02-07 00:59:46 +0800

    edit landing page

--- admin/landing_text.php
+++ admin/landing_text.php
@@ -1,5 +1,5 @@
 <?php
 // Store landing page text as PHP variables
-$welcomeText = "Welcome to the Jack of Fades Barbershop! HAHAAH";
+$welcomeText = "Welcome to the Jack of Fades Barbershop!";
 $headingText = "Affordable Price for a high quality haircut";
 $subheadingText = "Jack of Fades Barbershop offers high quality customized haircuts."; 

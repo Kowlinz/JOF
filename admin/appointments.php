9252d2bdacc2762f55b6fb6fa3341a12ce4ab62e
Kirby <kirbygonatice@gmail.com>
2025-02-13 00:15:06 +0800

    fixed js log error

--- admin/appointments.php
+++ admin/appointments.php
@@ -256,7 +256,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<script src="js/calendar.js"></script>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
